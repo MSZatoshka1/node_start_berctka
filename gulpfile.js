586773146ad4ad3d9b72ca9bed8ebc7baef5ae69
MSZatoshka1 <39450488+MSZatoshka1@users.noreply.github.com>
2018-09-07 23:46:51 +0400

    Update gulpfile.js
    www

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,12 @@ function css(){
     return gulp.src('public/css/man.css')
     .pipe(gcmq())
     // .pipe(cssmin())
-    .pipe(gulp.dest('public/css/'));
+    .pipe(gulp.dest('public/css/'))
+    .pipe(gulp.dest('sait/css/'));
+}
+function html(){
+    return gulp.src('./views/*.html')
+    .pipe(gulp.dest('sait/html/'));
 }
 function watch (){
     nodemon({
@@ -22,6 +27,7 @@ function watch (){
         tasks: ['browserify']
       })
     gulp.watch('./less/*.less', gulp.series(less_,css));
+    gulp.watch('./views/*.html', gulp.series(html));
 }
  
 const build = gulp.series(watch);
